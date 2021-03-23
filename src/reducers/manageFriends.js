ef8869e2af66eee3feb95ad40566fc21ef26acb8
Erik Huang <ekhu94@gmail.com>
2021-03-23 13:52:29 -0700

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    if (action.type === 'ADD_FRIEND') {
+        const newFriends = [...state.friends, action.friend];
+        return { friends: newFriends };
+    } else if (action.type === 'REMOVE_FRIEND') {
+        const updatedFriends = [...state.friends.filter(f => f.id !== action.id)];
+        return { friends: updatedFriends };
+    }
+    return state;
 }
